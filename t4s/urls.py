d03b6785a79a7cfae8630ee2aa5107bed154b5c4
mimic <itmustbemimic@gmail.com>
2022-05-14 00:10:14 +0900

    회원가입 페이지 추가

--- t4s/urls.py
+++ t4s/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = "t4s"
 urlpatterns = [
+    path('join/', views.join, name='join'),
     path('test/', views.login, name='login2'),
     path('success/', views.index, name='success'),
     path('', auth_views.LoginView.as_view(template_name='t4s/login.html'), name='login')
--- t4s/views.py
+++ t4s/views.py
@@ -1,11 +1,6 @@
-import logging
-import sys
-
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
-
-sys.path.append('..')
-logging.basicConfig(level=logging.DEBUG)
+from django.contrib import auth
 
 
 # login success page
@@ -13,14 +8,35 @@ def index(request):
     return HttpResponse("login success!")
 
 
+# 로그인 페이지
 def login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(request, username=username, password=password)
 
         print(request.POST)
 
-        return render(request, 't4s/login.html', {'data': 'aaaaa'})
+        if user is not None:
+            auth.login(request, user)
+            return redirect('t4s:success')
+        else:
+            return render(request, 't4s/login.html', {'error': '로그인 실패'})
 
-    elif request.method == 'GET':
+    else:
         return render(request, 't4s/login.html')
+
+
+# 회원가입 페이지
+def join(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        print(request.POST)
+        return render(request, 't4s/join.html')
+
+    else:
+        return render(request, 't4s/join.html')
